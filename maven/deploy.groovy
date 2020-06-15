4ec79030b3c475da8a48d03f9b4931cd1396aa1f
redskullk <56344108+redskullk@users.noreply.github.com>
2020-06-15 12:37:19 +0530

    Update deploy.groovy

--- maven/deploy.groovy
+++ maven/deploy.groovy
@@ -21,16 +21,16 @@ void getServers(String path) {
     def file = new File(path)
     dataval = jsonSlurper.parse(file)
     println "${dataval}"
+    return dataval
 }
 
-@NonCPS
 void call(app_env){
     run("pwd")
+    def x = getServers(app_env.serverInfo)
     stage("Deploy To dev"){
         println "Deploying to ${app_env.servers.size()} servers !!!"
         run("ls")
         try {
-            getServers(app_env.serverInfo)
             app_env.servers.each{ server ->
             println "deploying to ${server}"
             
