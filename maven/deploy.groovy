12633cdc5ee555f20c8125a80978cb6cd89ea175
redskullk <56344108+redskullk@users.noreply.github.com>
2020-06-15 13:17:53 +0530

    Update deploy.groovy

--- maven/deploy.groovy
+++ maven/deploy.groovy
@@ -5,11 +5,11 @@ def run(String cmd){
         sh "${cmd}"
      }
 }
-void deployToTomcat(String server){
+void deployToTomcat(server){
   println "deploying to tomcat server"
 }
 
-void deployToWas(String server){
+void deployToWas(server){
   println "deploying to WAS server"
 }
 
@@ -25,12 +25,11 @@ void call(app_env){
     stage("Deploy To dev"){
         println "Deploying to ${app_env.servers.size()} servers !!!"
         def data =  getServers "${app_env.serverInfo}"
-        println "${data}"
         app_env.servers.each{ server ->
             println "deploying to ${server}"
             def name = data[server].type
             if(name.equals("tomcat")){
-              deployToTomcat(server)
+              deployToTomcat(data[server])
             }
             if(name.equals("was")){
               deployToWas(server)
